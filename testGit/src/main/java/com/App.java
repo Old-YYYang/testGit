938bc545f2ab07f3655ac98c5b1447156eb997bd
Dongsu <ffddfd54254@gmail.com>
2020-11-05 23:08:47 +0800

    no message

--- testGit/src/main/java/com/App.java
+++ testGit/src/main/java/com/App.java
@@ -4,11 +4,8 @@ package com;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
         System.out.println( "test git" );
     }
---
+++ testGit/src/main/java/com/test.java
@@ -0,0 +1,8 @@
+package com;
+
+public class test {
+
+
+    private static final String attr = "TEST";
+
+}
